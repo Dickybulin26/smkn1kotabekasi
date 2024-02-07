c62519c9b44867772bbc6dc14f7e82d27c6eede0
Dicky_asqaelani26 <havidzgojek@gmail.com>
2024-02-07 14:19:15 +0700

    mengganti carousel pada hero section

--- code/script/slider.js
+++ code/script/slider.js
@@ -1,52 +1,93 @@
-function showSlide(card,dpl) {
+function showSlide(card,display) {
   card.items.forEach(item => {
     item.style.display = 'none';
   })
 
-  card.items[card.index].style.display = dpl;
+  card.items[card.index].style.display = display;
 }
 
-function nextSlide(card,dpl) {
+function nextSlide(card,display) {
   card.index = (card.index + 1) % card.items.length;
-  showSlide(card,dpl);
+  showSlide(card,display);
 }
-function previousSlide(card,dpl) {
+function previousSlide(card,display) {
   card.index = (card.index - 1 + card.items.length) % card.items.length;
-  showSlide(card,dpl);
+  showSlide(card,display);
 }
-function allslide(card,dpl){
+function allslide(card,display){
   card.items.forEach(item => {
-    item.style.display = dpl;
+    item.style.display = display;
   })
 }
-const carousel = [
-  {
+const component = {
+  // sliderHeroVer1 : {
+  //   items: document.querySelectorAll('.carousel-item'),
+  //   index: 0
+  // },
+  sliderHeroVer2 : {
     items: document.querySelectorAll('.carousel-item'),
     index: 0
   },
-  {
+  sliderPrestasi : {
     items: document.querySelectorAll('.carousel-item-auto'),
     index: 0
   }
-]
+}
 
-document.querySelector('.nextBtn').addEventListener('click', ()=>{
-  nextSlide(carousel[0],'block')
-})
-document.querySelector('.prevBtn').addEventListener('click', ()=>{
-  previousSlide(carousel[0],'block')
-});
+// document.querySelector('.nextBtn').addEventListener('click', ()=>{
+//   nextSlide(component.sliderHeroVer1,'block')
+// })
+// document.querySelector('.prevBtn').addEventListener('click', ()=>{
+//   previousSlide(component.sliderHeroVer1,'block')
+// });
+// showSlide(component.sliderHeroVer1,'block');
 
-showSlide(carousel[0],'block');
 
 function asdf(){
   if (window.matchMedia('screen and (max-width: 768px)').matches) {
-    nextSlide(carousel[1],'flex');
+    nextSlide(component.sliderPrestasi,'flex');
   }
   else {
-    allslide(carousel[1],'flex');
+    allslide(component.sliderPrestasi,'flex');
   }
 
   setTimeout(asdf,2000)
 }
 asdf()
+
+
+function showSlideVer2(card,cardHide) {
+  card.items.forEach(item => {
+    item.classList.remove('lebar')
+    item.classList.remove('hide')
+  })
+  card.items[card.index].classList.add('lebar')
+  card.items[cardHide].classList.add('hide')
+}
+
+function nextSlideVer2(card) {
+  card.index = (card.index + 1) % card.items.length;
+  cardHide = (card.index + 2) % card.items.length;
+  showSlideVer2(card,cardHide)
+}
+function previousSlideVer2(card) {
+  card.index = (card.index - 1 + card.items.length) % card.items.length;
+  cardHide = (card.index - 2 + card.items.length) % card.items.length;
+  showSlideVer2(card,cardHide)
+}
+const btnnextver2 = document.querySelector('.nextBtn')
+const btnpreviousver2 = document.querySelector('.prevBtn')
+function delay(btn) {
+  btn.disabled = true;
+  setTimeout(() => {
+    btn.disabled = false;
+  }, 1000);
+}
+btnnextver2.addEventListener('click', ()=>{
+  delay(btnnextver2)
+  nextSlideVer2(component.sliderHeroVer2)
+})
+btnpreviousver2.addEventListener('click', ()=>{
+  delay(btnpreviousver2)
+  previousSlideVer2(component.sliderHeroVer2)
+});
